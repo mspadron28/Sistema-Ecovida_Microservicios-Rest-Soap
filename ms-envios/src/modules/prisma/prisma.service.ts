8882361e58a123d5390d721b1c1d8eb059e463f2
David Cantuña <edcantuna@espe.edu.ec>
2024-11-30 23:36:40 -0500

    Se añade configuración de entorno con dotenv y validación de variables con Joi. Se actualiza el módulo Prisma para ser global y se modifica el servicio y archivo principal para utilizar el nuevo esquema de configuración.

--- ms-envios/src/modules/prisma/prisma.service.ts
+++ ms-envios/src/modules/prisma/prisma.service.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  private readonly logger = new Logger('ProductsService');
+  private readonly logger = new Logger('EnviosService');
   async onModuleInit() {
     await this.$connect();
     this.logger.log('Database connected successful');
