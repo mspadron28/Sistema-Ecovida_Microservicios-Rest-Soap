2e27beb0b2766d05628ec4a33d13034000ad74d3
mspadron28 <matiaspadron2@gmail.com>
2024-11-30 22:11:08 -0500

    Se añade el prisma client y se integra el modulo productos con prisma

--- ms-productos/src/app.module.ts
+++ ms-productos/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { ProductosModule } from './modules/productos/productos.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, ProductosModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
