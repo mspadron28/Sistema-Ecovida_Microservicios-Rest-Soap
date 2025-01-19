e779c01b4f3192c0e3278f3c26c1db10a6435fef
mspadron28 <matiaspadron2@gmail.com>
2025-01-19 09:27:09 -0500

    Re estructuración de carpetas backend y frontend

---
+++ backend/gateway-cliente/src/modules/auth/guards/role.guard.ts
@@ -0,0 +1,34 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    ForbiddenException,
+    Injectable,
+  } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '../lib/roles.enum';
+  
+  @Injectable()
+  export class RoleGuard implements CanActivate {
+    constructor(private readonly reflector: Reflector) {}
+  
+    canActivate(context: ExecutionContext): boolean {
+      // Obtener los roles requeridos desde el decorador
+      const requiredRoles = this.reflector.get<Role[]>('roles', context.getHandler());
+      if (!requiredRoles) {
+        return true; // Si no se definen roles, la ruta es pública
+      }
+  
+      // Obtener el usuario desde la solicitud
+      const request = context.switchToHttp().getRequest();
+      const user = request.user;
+  
+      if (!user || !user.roles) {
+        return false; // No hay usuario autenticado o no tiene roles asignados
+      }
+
+      
+      // Verificar si el usuario tiene al menos uno de los roles requeridos
+      return  requiredRoles.some((requiredRole) => user.roles.includes(requiredRole as string));
+    }
+  }
+  
---
+++ backend/ms-productos/src/modules/productos/dto/create-producto.dto.ts
@@ -0,0 +1,28 @@
+import { IsInt, IsString, IsDecimal, IsNotEmpty, Min, Max, IsBoolean, Matches } from 'class-validator';
+
+export class CreateProductoDto {
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-zA-Z0-9\sáéíóúÁÉÍÓÚñÑ.,-]+$/, {
+    message: 'El nombre contiene caracteres no permitidos.',
+  })
+  nombre: string;
+
+  @Matches(/^\d+(\.\d{1,2})?$/, {
+    message: 'El precio debe ser un número válido con hasta 2 decimales.',
+  })
+  @Min(0)
+  @IsNotEmpty()
+  precio: string; 
+
+  @IsInt()
+  @Min(0)
+  stock: number;
+
+  @IsInt()
+  @IsNotEmpty()
+  id_categoria: number;
+
+  @IsBoolean()
+  status?: boolean = true;
+}
